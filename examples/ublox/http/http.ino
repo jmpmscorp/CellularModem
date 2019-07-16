d40a7e3990d6cd833c3b6e8854c3b48d1312e824
Jose Manuel <jmpmscorp@hotmail.com>
2019-07-16 19:54:21 +0200

    Save Ublox http example

---
+++ examples/ublox/http/http.ino
@@ -0,0 +1,63 @@
+#define CELLMODEM_MODEL UBLOX
+#define CELLMODEM_USE_HTTP
+#include "CellularModem.h"
+
+
+#ifdef ARDUINO_SAMD_MKRGSM1400
+  #define modemSerial       SerialGSM
+  #define MODEM_DTR_PIN     GSM_DTR
+  #define MODEM_CST_PIN     -1
+  #define MODEM_ON_OFF_PIN  GSM_RESETN
+  #define MODEM_STATUS_PIN  -1
+
+  #define debugSerial       Serial
+#else
+  #warning "User should define his own pins"
+#endif
+
+
+
+CellularModem modem(modemSerial, MODEM_ON_OFF_PIN, MODEM_STATUS_PIN, MODEM_DTR_PIN, MODEM_CST_PIN); 
+CellularModemHttp http(modem);
+
+const char * server = "doleaguaycontrol.es";
+const int port = 80;
+
+
+void setup() {
+ 
+  debugSerial.begin(115200);
+  modemSerial.begin(115200);
+
+  unsigned int now = millis();
+
+  modem.init();
+  
+  modem.networkOn();
+
+  if(!modem.isGPRSConnected()) {
+    modem.attachGPRS("orangeworld", nullptr, nullptr);
+
+    http.initSSL(server);
+    http.get("/");
+  }
+}
+
+void loop() {
+  /* static unsigned long millisTime = millis();
+  
+  modem.poll();
+
+  if(millis() - millisTime > 10000) {
+    gprs.isConnected();
+    millisTime = millis();
+  } */
+
+  if (Serial.available()) {
+    SerialGSM.write(Serial.read());
+  }
+
+  if (SerialGSM.available()) {
+    Serial.write(SerialGSM.read());
+  }
+}
